cf7c425198d19d9513af6358ffbdb075cc3af535
Andrew Lovick <Github-Reneon@users.noreply.github.com>
2024-11-07 15:42:30 +0000

    Changes to be committed: 	new file:   .vscode/launch.json 	new file:   actions.go 	new file:   go.mod 	new file:   go.sum 	new file:   main.go

---
+++ actions.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Github-Reneon/dicetools"
+)
+
+func look() {
+	// -- Temp POC Implementation
+	fmt.Println("You see a verdant forest, with a river to the north.")
+}
+
+func exits() {
+	fmt.Println("north west (up)")
+}
+
+func attack() {
+	// Temp POC Implementation
+	roll := dicetools.RollNotation("1d100")
+
+	fmt.Println("Skill (Attack) beat an skill value of 100: roll was ", roll)
+
+	roll = dicetools.RollNotation("1d20")
+
+	fmt.Println("Attack beat an AC of 1: roll was ", roll)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module dragonmail
+
+go 1.21.3
+
+require github.com/Github-Reneon/dicetools v0.0.0-20231212221041-5a61a9e99347 // indirect
